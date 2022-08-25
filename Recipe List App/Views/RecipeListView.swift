e9f4f4a95682568a6762dad3c991c177bfbd0dc9
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-08-25 12:24:49 -0700

    Improve UI for recipe list - change list into scroll view to eliminate dividing lines - lazyVStack to save memory - replace nav bar title w/text element to improve consistency

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -14,30 +14,50 @@ struct RecipeListView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-
+        
         NavigationView {
-            List(model.recipes){ r in
+            
+            VStack(alignment: .leading){
                 
-                //make recipe elements tappable
-                NavigationLink(destination: RecipeDetailView(recipe:r),
-                    label: {
-                    
-                    //MARK: Row item
-                    HStack(spacing: 20.0){
-                    Image(r.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50, alignment: .center)
-                        .clipped()
-                        .cornerRadius(5)
-                    Text(r.name)
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                
+                //change list into a scrollview to eliminate lines between elements
+                ScrollView{
+                    LazyVStack(alignment: .leading) {
+                        
+                        ForEach(model.recipes){ r in
+                            //make recipe elements tappable
+                            NavigationLink(destination: RecipeDetailView(recipe:r),
+                                           label: {
+                                
+                                //MARK: Row item
+                                HStack(spacing: 20.0){
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(r.name)
+                                        .foregroundColor(.black)
+                                }
+                            })
+                            
+                            
+                            
+                        }
+                        
                     }
-                })
-                
-                
+                    
+                }
                 
             }
-            .navigationBarTitle("All Recipes")
+            //.navigationBarTitle("All Recipes")
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
         
     }
@@ -46,5 +66,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
