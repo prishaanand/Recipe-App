337c2ba0e35365f2c225241db736e53437cea652
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-08-25 11:38:24 -0700

    Create scrollable featured cards - Build UI in the RecipeFeaturedView

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -9,18 +9,75 @@ import SwiftUI
 
 struct RecipeFeaturedView: View {
     
-    //dont want to create a new model here, otherwise it will be hard to keep both sets of data in sync
-    //instead, want to have the same RecipeModel() instance powering both views
-    
     @EnvironmentObject var model:RecipeModel
     
+    //creates slidable cards for featured recipes
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 0) {
+        
+            Text("Featured Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+                
+            
+            GeometryReader { geo in
+            
+            TabView {
+                
+                // loop through recipes
+                ForEach (0..<model.recipes.count) { index in
+                    
+                    // show featured recipes
+                    if model.recipes[index].featured == true {
+                    
+                        // recipe card
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                            
+                            VStack(spacing: 0) {
+                                Image(model.recipes[index].image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipped()
+                                Text(model.recipes[index].name)
+                                    .padding(5)
+                            }
+                        }
+                        .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(15)
+                        .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                        
+                    }
+                }
+                
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
+        }
+        
+            VStack (alignment: .leading, spacing: 10) {
+                
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+            }
+            .padding([.leading, .bottom])
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
